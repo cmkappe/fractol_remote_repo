f4645882980289bfb3d623dfd2550898281d7ff4
Chiara Marie Kappe <ckappe@2-G-6.42heilbronn.de>
2025-03-19 15:08:59 +0100

    changed image to img to have shorter lines

--- fractol.h
+++ fractol.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chiarakappe <chiarakappe@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ckappe <ckappe@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 12:17:51 by chiarakappe       #+#    #+#             */
-/*   Updated: 2025/02/22 16:06:01 by chiarakappe      ###   ########.fr       */
+/*   Updated: 2025/03/19 15:03:46 by ckappe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,16 +39,14 @@ typedef	struct s_double_data
 	double	imaginary;
 }				t_double_data;
 
-
-typedef struct s_image
+typedef struct s_img
 {
-	void	*image_pointer;
+	void	*img_pointer;
 	char	*adress_pointer;
 	int		bits_per_pxl;
 	int		len_of_line;
 	int		endian;
-	
-}				t_image;
+}				t_img;
 
 typedef struct s_fractol
 {
@@ -57,22 +55,21 @@ typedef struct s_fractol
 	char	*title;
 	int		max_iterations;
 
-	t_image	image;
+	t_img	img;
 }				t_fractol;
 
-int		ft_abs(int n);
-int		ft_strcmp(char *str1, char *str2);
-int		handle_keypress(int keycode, t_fractol *fractol);
-int		close_window(t_fractol *fractol);
-void	my_mlx_pixel_put(t_image *data, int x, int y, int colour);
+int				ft_abs(int n);
+int				ft_strcmp(char *str1, char *str2);
+int				handle_keypress(int keycode, t_fractol *fractol);
+int				close_window(t_fractol *fractol);
+void			my_mlx_pixel_put(t_img *data, int x, int y, int colour);
 
-int		render(t_fractol *fractol);
-
+int				render(t_fractol *fractol);
 
 t_double_data	fractol_sum(t_double_data z1, t_double_data z2);
 t_double_data	fractol_sqr(t_double_data z);
 double			normalizing(double un_number, double new_min, double new_max,
-							double old_max);
+					double old_max);
 
 int				get_colour(int iter, t_fractol *fractol);
 
--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chiarakappe <chiarakappe@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ckappe <ckappe@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 11:50:57 by chiarakappe       #+#    #+#             */
-/*   Updated: 2025/02/22 16:09:29 by chiarakappe      ###   ########.fr       */
+/*   Updated: 2025/03/19 15:08:00 by ckappe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_abs(int n)
 	return (n);
 }
 
-int ft_strcmp(char *str1, char *str2)
+int	ft_strcmp(char *str1, char *str2)
 {
 	int	i;
 
@@ -33,22 +33,26 @@ int ft_strcmp(char *str1, char *str2)
 	return (0);
 }
 
-void	my_mlx_pixel_put(t_image *data, int x, int y, int colour)
+void	my_mlx_pixel_put(t_img *data, int x, int y, int colour)
 {
 	char	*dst;
 
-	dst = data->adress_pointer + (y * data->len_of_line + x * (data->bits_per_pxl / 8));
-	*(unsigned int*)dst = colour;
+	dst = data->adress_pointer + (y * data->len_of_line +
+			x * (data->bits_per_pxl / 8));
+	*(unsigned int *)dst = colour;
 }
 
 static void	fractol_init(t_fractol *fractol)
 {
 	fractol->mlx_pointer = mlx_init();
 	fractol->max_iterations = 100;
-	fractol->window_pointer = mlx_new_window(fractol->mlx_pointer, WIDTH, HEIGHT, fractol->title);
-	fractol->image.image_pointer = mlx_new_image(fractol->mlx_pointer, WIDTH, HEIGHT);
-	fractol->image.adress_pointer = mlx_get_data_addr(fractol->image.image_pointer, 
-		&fractol->image.bits_per_pxl, &fractol->image.len_of_line, &fractol->image.endian);
+	fractol->window_pointer = mlx_new_window(fractol->mlx_pointer,
+			WIDTH, HEIGHT, fractol->title);
+	fractol->img.img_pointer = mlx_new_image(fractol->mlx_pointer,
+			WIDTH, HEIGHT);
+	fractol->img.adress_pointer = mlx_get_data_addr(fractol->img.img_pointer,
+			&fractol->img.bits_per_pxl, &fractol->img.len_of_line,
+			&fractol->img.endian);
 }
 
 int	close_window(t_fractol *fractol)
@@ -57,7 +61,6 @@ int	close_window(t_fractol *fractol)
 	exit(EXIT_SUCCESS);
 }
 
-
 int	handle_keypress(int keycode, t_fractol *fractol)
 {
 	if (keycode == ESC_KEY)
@@ -65,12 +68,11 @@ int	handle_keypress(int keycode, t_fractol *fractol)
 	return (0);
 }
 
-
 int main(int ac, char **av)
 {
 	t_fractol	fractol;
-	
-	if (!((ac == 2 && !ft_strcmp(av[1], "Mandelbrot")) 
+
+	if (!((ac == 2 && !ft_strcmp(av[1], "Mandelbrot"))
 		|| (ac == 4 && !ft_strcmp(av[1], "Julia"))))
 	{
 		printf("Wrong input, please try again.");
@@ -78,12 +80,12 @@ int main(int ac, char **av)
 	}
 	fractol.title = av[1];
 	fractol_init(&fractol);
-	
+
 	mlx_hook(fractol.window_pointer, 2, 1L << 0, handle_keypress, &fractol);
 	mlx_hook(fractol.window_pointer, 17, 1L << 17, close_window, &fractol);
 
 	render(&fractol);
-	
+
 	mlx_loop(fractol.mlx_pointer);
 
 
