dce716c3fcd5eca200fdff07a98b49c2042713cd
Chiara Marie Kappe <ckappe@2-G-6.42heilbronn.de>
2025-04-02 17:22:47 +0200

    added burning ship

--- src/render_fractol.c
+++ src/render_fractol.c
@@ -6,7 +6,7 @@
 /*   By: ckappe <ckappe@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 15:28:31 by chiarakappe       #+#    #+#             */
-/*   Updated: 2025/03/28 19:32:08 by ckappe           ###   ########.fr       */
+/*   Updated: 2025/04/02 17:21:48 by ckappe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ int	get_colour(int iter, t_fractol *fractol)
 	if (iter == MAX_ITER)
 		return (COLOUR_BLACK);
 	t = (double)iter / fractol->max_iterations;
-
 	r = (int)(252 * (1 - t) * t * 4);
 	g = (int)(190 * (1 - t) * (1 - t) * t * 6);
 	b = (int)(17 + 50 * (10 * t));
@@ -32,29 +31,48 @@ int	get_colour(int iter, t_fractol *fractol)
 	return (r << 16 | g << 8 | b);
 }
 
-static int iterations(int x, int y, t_fractol *fractol)
+static	t_double_data	pick_fractol(t_fractol *fractol, t_double_data z_imag)
 {
-	int	colour;
-	t_double_data	z_imag;
 	t_double_data	c_const;
-	int				iterations;
 
-	z_imag.real = normalizing(x, -2, 2, WIDTH) / fractol->zoom + fractol->offset_x;
-	z_imag.imaginary = normalizing(y, -2, 2, HEIGHT) / fractol->zoom + fractol->offset_y;
-	if (ft_strcmp(fractol->title, "Mandelbrot") == 0)
+	if (ft_strcmp(fractol->title, "Julia") == 0)
+	{
+		c_const.real = fractol->julia_real;
+		c_const.imaginary = fractol->julia_imag;
+	}
+ 	else
 	{
 		c_const = z_imag;
 		z_imag.real = 0;
 		z_imag.imaginary = 0;
 	}
-	else
+	return (c_const);
+}
+
+static t_double_data burning(t_fractol *fractol, t_double_data z_imag)
+{
+	if (ft_strcmp(fractol->title, "Burning ship") == 0)
 	{
-		c_const.real = fractol->julia_real;
-		c_const.imaginary = fractol->julia_imag;
+		z_imag.real = ft_abs(z_imag.real);
+		z_imag.imaginary = ft_abs(z_imag.imaginary);
 	}
+	return (z_imag);
+}
+
+static int	iterations(int x, int y, t_fractol *fractol)
+{
+	int				colour;
+	t_double_data	z_imag;
+	t_double_data	c_const;
+	int				iterations;
+
+	z_imag.real = normal(x, -2, 2, WIDTH) / fractol->zoom + fractol->offset_x;
+	z_imag.imaginary = normal(y, -2, 2, HEIGHT) / fractol->zoom + fractol->offset_y;
+	c_const = pick_fractol(fractol, z_imag);
 	iterations = 0;
 	while (iterations <= fractol->max_iterations)
 	{
+		z_imag = burning(fractol, z_imag);
 		z_imag = fractol_sum(fractol_sqr(z_imag), c_const);
 		if ((z_imag.real * z_imag.real + z_imag.imaginary * z_imag.imaginary) > 4)
 		{
@@ -84,24 +102,7 @@ int	render(t_fractol *fractol)
 		}
 		y++;
 	}
-	mlx_put_image_to_window(fractol->mlx_pointer, fractol->window_pointer, fractol->img.img_pointer, 0, 0);
+	mlx_put_image_to_window(fractol->mlx_pointer, fractol->window_pointer,
+		fractol->img.img_pointer, 0, 0);
 	return (0);
 }
-
-// void zoom(t_fractol *fractol, int mouse_x, int mouse_y, double zoom_factor)
-// {
-
-// 	(void)mouse_y;
-// 	(void)mouse_x;
-
-//     // double normalized_x = normalizing(mouse_x, -2, 2, WIDTH);
-//     // double normalized_y = normalizing(mouse_y, -2, 2, HEIGHT);
-
-//     // fractol->offset_x += (normalized_x - fractol->offset_x) * (1 - zoom_factor);
-//     // fractol->offset_y += (normalized_y - fractol->offset_y) * (1 - zoom_factor);
-//     // fractol->zoom *= zoom_factor;
-
-// 	printf("Zoom called: mouse_x=%d, mouse_y=%d, zoom_factor=%f\n", mouse_x, mouse_y, zoom_factor);
-// 	printf("New offset_x=%f, offset_y=%f, zoom=%f\n", fractol->offset_x, fractol->offset_y, fractol->zoom);
-// }
-
