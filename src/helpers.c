e8a73d601db201ff8792c580489735894456819a
Chiara Marie Kappe <ckappe@2-G-6.42heilbronn.de>
2025-04-04 18:27:20 +0200

    added atof and a helper to handle the signs

--- src/helpers.c
+++ src/helpers.c
@@ -6,11 +6,13 @@
 /*   By: ckappe <ckappe@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 15:38:10 by ckappe            #+#    #+#             */
-/*   Updated: 2025/04/02 17:15:37 by ckappe           ###   ########.fr       */
+/*   Updated: 2025/04/04 17:58:32 by ckappe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
+
+
 
 int	ft_strcmp(char *str1, char *str2)
 {
@@ -32,4 +34,46 @@ int	close_window(t_fractol *fractol)
 	// mlw_destroy_image(fractol);
 	// mlx_destroy_window(fractol);
 	exit(EXIT_SUCCESS);
+}
+
+static int	handle_sign(char *num, int *i)
+{
+	int	sign;
+
+	sign = 1;
+	if (num[*i] == '-' || num[*i] == '+')
+	{
+		if (num[*i] == '-')
+			sign = -1;
+		(*i)++;
+	}
+	return (sign);
+}
+
+double	ft_atof(char *num)
+{
+	double	result;
+	double	fraction;
+	int		sign;
+	int		i;
+	double	divisor;
+
+	result = 0.0;
+	fraction = 0.0;
+	i = 0;
+	sign = handle_sign(num, &i);
+	while (num[i] >= '0' && num[i] <= '9')
+		result = result * 10 + (num[i++] - '0');
+	if (num[i] == '.')
+	{
+		i++;
+		divisor = 10.0;
+		while (num[i] >= '0' && num[i] <= '9')
+		{
+			fraction += (num[i] - '0') / divisor;
+			divisor *= 10.0;
+			i++;
+		}
+	}
+	return ((result + fraction) * sign);
 }
