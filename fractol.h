6c15ac14a36ee2ac83665655c6d2e560482e45a8
Chiara Marie Kappe <ckappe@2-G-8.42heilbronn.de>
2025-04-08 17:01:01 +0200

    finished project

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: ckappe <ckappe@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 12:17:51 by chiarakappe       #+#    #+#             */
-/*   Updated: 2025/04/06 17:37:51 by ckappe           ###   ########.fr       */
+/*   Updated: 2025/04/08 16:41:14 by ckappe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@
 # define KEY_MINUS 78
 # define SCROLL_UP 4
 # define SCROLL_DOWN 5
-# define KEY_C 8
 
 # define COLOUR_BLACK 0x000000
 
@@ -65,25 +64,26 @@ typedef struct s_fractol
 	t_img	img;
 }				t_fractol;
 
-
-void			my_mlx_pixel_put(t_img *data, int x, int y, int colour);
-
+// ------------------------------- helpers --------------------------------//
 double			ft_abs(double n);
 int				ft_strcmp(char *str1, char *str2);
 int				close_window(t_fractol *fractol);
 double			ft_atof(char *num);
 int				julia_in(char *num);
 
+// --------------------- handle key & mouse events ------------------------//
 int				mouse_hook(int button, int x, int y, t_fractol *fractol);
 int				handle_keypresses(int keycode, t_fractol *fractol);
-//int				key_hook(int keycode, t_fractol *fractol);
 
+// ------------------------------- maths ---------------------------------//
 t_double_data	fractol_sum(t_double_data z1, t_double_data z2);
 t_double_data	fractol_sqr(t_double_data z);
 double			normal(double un_number, double new_min, double new_max,
 					double old_max);
 
+// ------------------------------- render --------------------------------//
 int				get_colour(int iter, t_fractol *fractol);
 int				render(t_fractol *fractol);
+void			my_mlx_pixel_put(t_img *data, int x, int y, int colour);
 
 #endif
